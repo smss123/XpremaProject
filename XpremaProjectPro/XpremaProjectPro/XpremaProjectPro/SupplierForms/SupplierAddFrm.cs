675bf3382910ba2800205105b5209b753e253195
smss123 <smsssamer@live.com>
2014-08-16 16:12:31 +0400

    Add Supplier finshed Coding

--- XpremaProjectPro/XpremaProjectPro/XpremaProjectPro/SupplierForms/SupplierAddFrm.cs
+++ XpremaProjectPro/XpremaProjectPro/XpremaProjectPro/SupplierForms/SupplierAddFrm.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
-
+using Xprema.Base;
 namespace XpremaProjectPro.SupplierForms
 {
     public partial class SupplierAddFrm : DevExpress.XtraEditors.XtraForm
@@ -16,6 +16,66 @@ namespace XpremaProjectPro.SupplierForms
         public SupplierAddFrm()
         {
             InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.Validation())
+                {
+                    Xprema_PrjectEntities db = new Xprema_PrjectEntities();
+                    Supplier sup = new Supplier()
+                    {
+                        Adderss = adderssTextBox.Text,
+                        Email = emailTextBox.Text,
+                        Fax = faxTextBox.Text,
+                        name = nameTextBox.Text,
+                        PhoneNumber = phoneNumberTextBox.Text,
+                        SuppliersNatural = suppliersNaturalTextBox.Text
+                    };
+                    db.Suppliers.Add(sup);
+                    db.SaveChangesAsync();
+                    MessageBox.Show("Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+
+        private bool Validation()
+        {
+            bool state = true;
+            //Supplier sup = new Supplier()
+            //{
+            //    Adderss = adderssTextBox.Text,
+            //    Email = emailTextBox.Text,
+            //    Fax = faxTextBox.Text,
+            //    name = nameTextBox.Text,
+            //    PhoneNumber = phoneNumberTextBox.Text,
+            //    SuppliersNatural = suppliersNaturalTextBox.Text
+            //};
+            state = ChangeToError(state);
+            if (emailTextBox.Text="")
+            {
+                
+            }
+
+            return state;
+        }
+
+        private bool ChangeToError(TextEdit txt)
+        {
+            if (txt.Text == "")
+            {
+                txt.Text = "";
+                txt.BackColor = Color.Red; //Color.FromArgb(255, 255, 192);
+                return false;
+            }
+            return true;
         }
     }
 }
